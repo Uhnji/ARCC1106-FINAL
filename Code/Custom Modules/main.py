2fc4657ff5bb41bae18c66ed463f1f447fe67b9a
Uhnji <79808078+Uhnji@users.noreply.github.com>
2023-03-29 09:52:37 -0500

    Update main.py

--- Code/Custom Modules/main.py
+++ Code/Custom Modules/main.py
@@ -1,1 +1,25 @@
 import pygame
+
+if __name__ == '__main__':
+    pygame.init()
+
+    screen = pygame.display.set_mode((1280,720))
+
+    clock = pygame.time.Clock()
+
+    while True:
+        for event in pygame.event.get(): # Player Input Handler
+            if event.type == pygame.QUIT:
+                pygame.quit()
+                raise SystemExit
+
+        # Do logical updates here.
+        # ...
+
+        screen.fill("black")  # Background color
+
+        # Render the graphics here.
+        # ...
+
+        pygame.display.flip()  # Refresh on-screen display
+        clock.tick(60)         # wait until next frame (at 60 FPS)

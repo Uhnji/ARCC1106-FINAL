91a93d58d22969a5fe7c7b287b1ce5f998d95248
Uhnji <79808078+Uhnji@users.noreply.github.com>
2023-03-29 23:43:00 -0500

    Update to Main.py V0.1 - MOVEMENT TEST
    + Switched to fullscreen
    + Implemented basic movement
    + Implemented Dash
    + Prepared Native for Full-Screen Menu
    
    TODO: 
    + Initialize Collision and run tests
    + Implement Sprites

--- Code/Custom Modules/main.py
+++ Code/Custom Modules/main.py
@@ -1,25 +1,72 @@
 import pygame
+import sys
+import os
+from player import Player_Object
 
-if __name__ == '__main__':
+def initialize_pygame(): 
+    """Sets starting parameters, WIDTH, HEIGHT, and TITILE parameters and instantilizes PyGame"""
+    TITLE = "CSCI_FINAL V.0.1 - MOVEMENT TEST"
+   
+    win = pygame.display.set_mode((0, 0), pygame.FULLSCREEN)
+    pygame.display.set_caption(TITLE)
     pygame.init()
 
-    screen = pygame.display.set_mode((1280,720))
-
     clock = pygame.time.Clock()
+    return win,clock
+
+
+if __name__ == '__main__':
+    win, clock = initialize_pygame()
+    
+    player = Player_Object(0,0)
 
     while True:
-        for event in pygame.event.get(): # Player Input Handler
+        for event in pygame.event.get():
             if event.type == pygame.QUIT:
                 pygame.quit()
-                raise SystemExit
+                sys.exit()
 
-        # Do logical updates here.
-        # ...
+            keys = pygame.key.get_pressed()
 
-        screen.fill("black")  # Background color
+            if keys[pygame.K_a] or keys[pygame.K_LEFT]:
+                player.left = True
+            else:
+                player.left = False
 
-        # Render the graphics here.
-        # ...
 
-        pygame.display.flip()  # Refresh on-screen display
-        clock.tick(60)         # wait until next frame (at 60 FPS)
+
+            if keys[pygame.K_d] or keys[pygame.K_RIGHT]:
+                player.right = True
+            else:
+                player.right = False
+
+
+
+            if keys[pygame.K_w] or keys[pygame.K_UP]:
+                player.up = True
+            else:
+                player.up = False
+
+
+
+            if keys[pygame.K_s] or keys[pygame.K_DOWN]:
+                player.down = True
+            else:
+                player.down = False
+
+
+
+            if keys[pygame.K_SPACE] or keys[pygame.K_RCTRL]:
+                player.dash_active = True
+            else:
+                player.dash_active = False
+
+
+        win.fill((12,24,36))
+        player.draw(win)
+
+        player.update()
+        pygame.display.flip()
+        clock.tick(120)
+
+

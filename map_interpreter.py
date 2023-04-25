d96415fe7ee7d07d17fe14896c6f2b0e4db23b5d
Uhnji <79808078+Uhnji@users.noreply.github.com>
2023-04-25 10:16:02 -0500

    Update map_interpreter.py
    Refer to Main for changes

--- map_interpreter.py
+++ map_interpreter.py
@@ -11,8 +11,10 @@ class TileMap:
         self.x0, self.y0 = 0, 0
         self.x, self.y = self.x0, self.y0
 
-        self.demo_tile = pygame.image.load("Sprites/Demo_Tile.png")
+        self.demo_tile = pygame.image.load("Sprites/Terrain Tiles/Demo_Tile.png")
         self.demo_tile = pygame.transform.scale(self.demo_tile, (self.tile_size, self.tile_size))
+
+        self.floor_tile = pygame.image.load("Sprites/Terrain Tiles/FloorTile.png")
 
     def read(self):#Run through each nested list in the map data, and store a tile in tiles list when map[i] = 1.
 
@@ -20,9 +22,11 @@ class TileMap:
 
             for j in range(len(self.map[i])):
 
+                if self.map[i][j] == 0:
+                    self.tiles.append([self.floor_tile, self.x, self.y,0])
                 if self.map[i][j] == 1:
 
-                    self.tiles.append([self.demo_tile, self.x, self.y])
+                    self.tiles.append([self.demo_tile, self.x, self.y,1])
 
                 self.x += self.tile_size
 
